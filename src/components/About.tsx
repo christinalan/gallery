290526168a8cfe2b8fef37efea6742409e1826f4
christina lan <christina.lan13@gmail.com>
2023-12-11 14:54:11 -0500

    adding styling to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,9 @@ import './style.css';
 const About = () => {
     return (
         <div className="project text-lg sm:text-sm max-w-xl">A selection of projects I've enjoyed working on, ranging from development and design to
-        production, editing, and left-field sonics. This site was built with React, React Router, Typescript, Tailwind, and GSAP. Into it All.
+        production, editing, and left-field sonics. Into it all.
             <br/><br />Please email me at christina.lan@gmail.com if you'd like to get in touch.
+            <br /><br /><p className="text-xs italic mt-5">Built with React, React Router, Typescript, Tailwind, and GSAP.</p>
         </div>
     )
 }
