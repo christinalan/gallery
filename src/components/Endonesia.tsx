7802bc1d5a9c917ccefdf9628a24c0b97de29767
christina lan <christina.lan13@gmail.com>
2023-12-29 10:21:05 +0000

    building the endonesia tab

--- src/components/Endonesia.tsx
+++ src/components/Endonesia.tsx
@@ -5,7 +5,7 @@ import endonesia from '../assets/endonesia.png'
 const Endonesia = () => {
     const projectData = {
         stack: "React, React Router, Typescript, Soundcloud, Figma",
-        link: "https://endonesia.web.app/mixes",
+        link: "https://endonesia.web.app",
         description: `Website for a NYC and Seoul based label and mix series, Endonesia. This project involved building a 
         custom music player with Soundcloud's widget, and incorporating Bandcamp API for label releases.`,
         logo: endonesia_logo,
